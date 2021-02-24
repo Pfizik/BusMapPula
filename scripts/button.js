e0d43b9a9b9c30e74d0d6ea38d3130018beb8808
Pfizik <viksi2806@gmail.com>
2021-02-24 07:38:16 +0100

    Mobile fix

--- scripts/button.js
+++ scripts/button.js
@@ -1,18 +1,14 @@
 function mapAppear(){
-    document.getElementById("gmap").className = "gmap-active";
-    document.getElementById("gmap").setAttribute("src", full_url);
+    document.getElementById("map-div").className = "mapdiv-active";
     document.getElementById("map-switch").className= "switch-active";
-    document.getElementById("map-exit").className= "exit-active";
     document.getElementById("map-switch").removeEventListener("click", mapAppear);
     document.getElementById("map-switch").addEventListener("click", mapDisappear);
     document.getElementById("map-exit").addEventListener("click", mapDisappear);
 }
 
 function mapDisappear(){
-    document.getElementById("gmap").className = "gmap-inactive";
-    document.getElementById("gmap").setAttribute("src", "");
+    document.getElementById("map-div").className = "mapdiv-inactive";
     document.getElementById("map-switch").className= "switch-inactive";
-    document.getElementById("map-exit").className= "exit-inactive";
     document.getElementById("map-switch").removeEventListener("click", mapDisappear);
     document.getElementById("map-switch").addEventListener("click", mapAppear);
     document.getElementById("map-exit").removeEventListener("click", mapDisappear);
